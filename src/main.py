8daa2ba9fe4f989e84bd8a93f98e545b55803b86
Sergei <vavilin.dev@gmail.com>
2025-07-24 15:10:37 +0300

    Appended CORS MIDDLEWARE

--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.api.v1 import endpoints
 
 app = FastAPI(
-    title="FastAPI Template",
+    title="FastAPI Test Salamat",
     version="0.1.0",
 )
 
 app.include_router(endpoints.router, prefix="/api/v1")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
