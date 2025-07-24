cb4d630860af39ae999876bf940e2b5347815ae5
Sergei <vavilin.dev@gmail.com>
2025-07-24 15:10:07 +0300

    remove SYSTEM_PROMPT

--- src/api/v1/endpoints.py
+++ src/api/v1/endpoints.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage
 
-from src.common.llm_model import LLM, SYSTEM_PROMPT
+from src.common.llm_model import LLM
 
 router: APIRouter = APIRouter()
 
 
-@router.get("/ask_llm")  # type: ignore
+@router.get("/ask_llm")
 async def ask_agent(human_input: str) -> dict[str, str]:
     messages = [
-        SystemMessage(content=SYSTEM_PROMPT),
         HumanMessage(content=human_input),
     ]
     answer = LLM.invoke(messages).content
