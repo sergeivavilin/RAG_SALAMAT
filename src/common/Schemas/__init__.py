bf16d06a88c9cff249cd038c4c6091b9799e92f7
Sergei <vavilin.dev@gmail.com>
2025-07-24 03:27:39 +0300

    imported Schemas and Models into init.py

---
+++ src/common/Schemas/__init__.py
@@ -0,0 +1,17 @@
+from src.common.Schemas.pharmacy_schemas import (
+    Client,
+    ItemOrder,
+    Order,
+    PharmacyProductSchema,
+    PharmacySchema,
+    ProductSchema,
+)
+
+__all__ = [
+    "Client",
+    "ItemOrder",
+    "Order",
+    "PharmacySchema",
+    "PharmacyProductSchema",
+    "ProductSchema",
+]
---
+++ src/db/Models/__init__.py
@@ -0,0 +1,3 @@
+from src.db.Models.pharmacy_models import Base, Pharmacy, PharmacyProduct, Product
+
+__all__ = ["Base", "Pharmacy", "PharmacyProduct", "Product"]
