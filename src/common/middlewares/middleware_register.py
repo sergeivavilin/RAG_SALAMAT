2ed7e4e207aad4243d95cdde6af3f02b00bc9c6b
Sergei <vavilin.dev@gmail.com>
2025-08-01 02:01:25 +0300

    log middleware deleted

---
+++ src/common/middlewares/middleware_register.py
@@ -0,0 +1,77 @@
+import logging
+import time
+from typing import Callable, Awaitable
+
+from fastapi import Request, Response, FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from src.common.logger import logger
+
+
+async def debug_middleware(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    start_decoding_time = time.perf_counter()
+    # Считаем чистое время ответа
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    response_process_time = round(time.perf_counter() - start_time, 3)
+
+    # Считываем тело ответа и собираем заново
+    resp_body = b""
+    async for chunk in response.body_iterator:  # type: ignore
+        resp_body += chunk
+
+    response_body = resp_body.decode("utf-8", errors="ignore")
+
+    response = Response(
+        content=resp_body,
+        status_code=response.status_code,
+        headers=dict(response.headers),
+        media_type=response.media_type,
+    )
+    total_decoding_time = round(time.perf_counter() - start_decoding_time, 3)
+    # Логируем запрос и ответ
+    request_body = await request.json()
+    logger.debug(
+        "--> Request from %s %s to %s - request body: %s",
+        request.client,
+        request.method,
+        request.url,
+        request_body,
+    )
+    logger.debug(
+        "<-- Response took %s seconds - total time response took %s seconds - response body: %s",
+        response_process_time,
+        total_decoding_time,
+        response_body,
+    )
+
+    return response
+
+
+async def log_new_request_middleware(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    if logger.isEnabledFor(logging.DEBUG):
+        return await debug_middleware(request, call_next)
+    logger.info(
+        "Request from %s %s to %s",
+        request.client,
+        request.method,
+        request.url,
+    )
+    return await call_next(request)
+
+
+def register_middlewares(app: FastAPI) -> None:
+    app.middleware("http")(log_new_request_middleware)
+    # app.middleware("http")(debug_middleware)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
