1d0ce73d6d114cd32eb26cd6a58e919569f5e265
Sergei <vavilin.dev@gmail.com>
2025-07-31 18:18:07 +0300

    Log_middleware refactored (split debug and warnings logging)

--- src/common/middlewares/log_middleware.py
+++ src/common/middlewares/log_middleware.py
@@ -15,27 +15,38 @@ async def log_middleware(
 
         start_time = time.perf_counter()
         body = await request.json()
-
-        logger.debug(
-            f"--> Incoming request: {request.method} {request.url} "
-            f"- User input: {body.get('user_input')} "
-            f"- User id: {body.get('thread_id')}"
-        )
+        if request.client:
+            logger.debug(
+                f"--> Incoming request: {request.method} {request.url} "
+                f"- User input: {body.get('user_input')} "
+                f"- User id: {body.get('thread_id')} "
+                f"- Request from: {request.client.host}:{request.client.port} "
+            )
+        else:
+            logger.warning(
+                f"--> Incoming request: {request.method} {request.url} "
+                f"- User input: {body.get('user_input')} "
+                f"- User id: {body.get('thread_id')} "
+                f"- Request from: CLIENT ip:port NOT FOUND "
+            )
         # Перехватим ответ
         response = await call_next(request)
 
         # Считаем тело (нужно прочитать и пересобрать)
         resp_body = b""
+
         async for chunk in response.body_iterator:  # type: ignore
             resp_body += chunk
-        body = response.body
+
+        response_body = resp_body.decode("utf-8", errors="ignore")
         process_time = time.perf_counter() - start_time
+
         # Логируем
         logger.debug(
             f"<-- Outgoing response: {request.method} {request.url} "
             f"- Status: {response.status_code} "
-            f"- Body: {body} "
-            f"- process time: {process_time}"
+            f"- ResponseBody: {response_body} "
+            f"- Process time: {process_time}"
         )
 
         # Возвращаем новый response с тем же телом
@@ -57,15 +68,14 @@ async def log_middleware(
         host = request.client.host if request.client else ""
         port = request.client.port if request.client else ""
         log_dict = {
-            "method": method,
-            "url": url,
-            "request from": f"{host}:{port}",
-            "response status code": response.status_code,
-            "process time": process_time,
+            "Method": method,
+            "Url": url,
+            "Request from": f"{host}:{port}",
+            "Response status code": response.status_code,
+            "Process time": process_time,
         }
         logger.info(
             log_dict,
-            exc_info=True,
             extra=log_dict,
         )
         return response
