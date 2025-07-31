8e4b673b90ee8bd5d8f64aa2b1589232a004db31
Sergei <vavilin.dev@gmail.com>
2025-07-31 18:18:52 +0300

    time_rotating_file_handler appended to root logger

--- src/common/logger.py
+++ src/common/logger.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-
+from logging.handlers import TimedRotatingFileHandler
 
 logger = logging.getLogger(__name__)
 
@@ -12,11 +12,26 @@ formater = logging.Formatter(
 
 # Handlers
 stream_handler = logging.StreamHandler(sys.stdout)
-file_handler = logging.FileHandler("logs/root_logger.log", encoding="utf-8", mode="w")
+
+file_handler = logging.FileHandler("logs/root_logger.log", encoding="utf-8", mode="a")
+
+time_rotating_file_handler = TimedRotatingFileHandler(
+    "logs/app.log",
+    when="midnight",  # ротация каждый день в полночь
+    interval=1,
+    backupCount=7,  # хранить 7 последних файлов
+)
+time_rotating_file_handler.suffix = "%Y-%m-%d"  # добавит дату в имя файла
+
 
 stream_handler.setFormatter(formater)
 file_handler.setFormatter(formater)
+time_rotating_file_handler.setFormatter(formater)
 
-logger.handlers = [stream_handler, file_handler]
+logger.handlers = [
+    stream_handler,
+    # file_handler,
+    time_rotating_file_handler,
+]
 
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
